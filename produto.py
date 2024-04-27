f35bf378b3555ac8acd14536b23f8e427e570635
Gabriel "Syll3x" Ponzoni <gabrielponzoni1234@gmail.com>
2024-04-27 18:26:50 -0300

    3.0
    populated .txt files

---
+++ produto.py
@@ -0,0 +1,5 @@
+class Produto:
+    def __init__(self, codigo: int, nome: str, preco: float):
+        self.__codigo = codigo
+        self.__nome = nome
+        self.__preco = preco
---
+++ quarto.py
@@ -0,0 +1,3 @@
+class Quarto:
+    def __init__(self, numero: int, categoria: str, diaria: float, consumo: list[int]):
+        pass
